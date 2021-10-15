4789719bef81b5b53d66f6a8d649b7ed6df38238
Benjamin ADOLPHE <adolphe906@gmail.com>
2021-10-15 22:36:09 +0200

    Ajout de l'image + début de l'UI

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -10,33 +10,50 @@ import '../model/question.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key);
-  final QuestionsFirebaseProvider _questionsFirebaseProvider = new QuestionsFirebaseProvider();
+  final QuestionsFirebaseProvider _questionsFirebaseProvider =
+      new QuestionsFirebaseProvider();
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _questionsFirebaseProvider.getAllQuestions(),
-      builder: (context,snapshot){
-        if(snapshot.hasError){
-          return ErrorView(error: snapshot.error.toString());
-        }
-        if(snapshot.hasData){
-          return QuestionsView(questions: snapshot.data! as List<Question>);
-        }
-        return Loading();
-      });
-  }
+    return Scaffold(
+      body: Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      color: Colors.blueGrey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            "Bienvenue dans Question / Réponse",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: MediaQuery.of(context).size.width * 0.05,
+            ),
+          ),
+          SizedBox(height: MediaQuery.of(context).size.height*0.15,),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: Image(
+              image: AssetImage("images/homeImage.png"),
+            ),
+          ),
 
-  List<Question> getQuestionsFromSnapshot(
-      AsyncSnapshot<QuerySnapshot> snapshot) {
-    List<Question> res = [];
-    if (snapshot.hasData) {
-      snapshot.data!.docs.forEach((element) {
-        if (element.exists) {
-          res.add(Question.fromJson(element.data()));
-        }
-      });
-    }
-    return res;
+          // FutureBuilder(
+          //     future: _questionsFirebaseProvider.getAllQuestions(),
+          //     builder: (context, snapshot) {
+          //       if (snapshot.hasError) {
+          //         return ErrorView(error: snapshot.error.toString());
+          //       }
+          //       if (snapshot.hasData) {
+          //         return QuestionsView(
+          //             questions: snapshot.data! as List<Question>);
+          //       }
+          //       return Loading();
+          //     })
+        ],
+      ),
+    ),
+    );
   }
 }
