c28fa5acf4d7de40a7edcaeb2e5ea9a4fb1b95f0
Benjamin ADOLPHE <adolphe906@gmail.com>
2021-10-16 00:49:32 +0200

    ignore: import_of_legacy_library_into_null_safe

--- lib/views/screen/firebase_init_view.dart
+++ lib/views/screen/firebase_init_view.dart
@@ -1,3 +1,4 @@
+// ignore: import_of_legacy_library_into_null_safe
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:questions_reponses/views/widget/error_view.dart';
