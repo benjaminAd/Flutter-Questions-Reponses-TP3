493e4be96446f99f4091fc8bc4e244110f0aa66d
benjamin <adolphe906@gmail.com>
2021-10-17 20:56:39 +0200

    remove realtime read
    Realtime read caused bugs sometimes
    couldn't get the themes ...

--- lib/data/repositories/questions_repositories.dart
+++ lib/data/repositories/questions_repositories.dart
@@ -1,14 +1,15 @@
 // ignore_for_file: import_of_legacy_library_into_null_safe
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:questions_reponses/Utils/constants.dart';
 
 class QuestionsRepository {
   var _questionsdb =
       FirebaseFirestore.instance.collection(Constants.questions_reference);
 
-  Stream<QuerySnapshot> getAllQuestions() {
-    return _questionsdb.snapshots();
+  Future<QuerySnapshot> getAllQuestions() {
+    return _questionsdb.get();
   }
 
   Future<QuerySnapshot> getQuestionsFromTheme(String theme){
